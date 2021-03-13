4dbf344d7f83420dda1eb9c8e18621bbc75a2ccc
Augusto <vilaca.aug@gmail.com>
2021-03-12 21:50:46 -0300

    Commit 4

--- video-locadora/Rental.java
+++ video-locadora/Rental.java
@@ -15,4 +15,22 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    private double getCharge() {
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+    }
 }
